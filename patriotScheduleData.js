f27a9cda5728b32adfac5c2968f39f6e09402d4b
rsleeper1 <39035524+rsleeper1@users.noreply.github.com>
2018-12-06 18:00:00 -0600

    Week 7 Updates

--- patriotScheduleData.js
+++ patriotScheduleData.js
@@ -1,32 +1,34 @@
-var divStandings = (function(){
-    var team1 = {
-        name: 'New England Patriots',
-        overallWins: 9,
-        divWins: 3, 
-    };
-    var team2 = {
-        name: 'Miami Dolphins',
-        overallWins: 6,
-        divWins: 3, 
-    };
-    var team3 = {
-        name: 'Buffalo Bills',
-        overallWins: 4,
-        divWins: 1,
-    };
-    var team4 = {
-        name: 'New York Jets',
-        overallWins: 3,
-        divWins: 0,
-    };
-    var intOrder = [team1.overallWins, team2.overallWins, team3.overallWins, team4.overallWins];
-
-    function sortTeams(a, b){
-        return b-a;
+<script>
+    function divStandings(){
+        var team1 = {
+            name: 'New England Patriots',
+            overallWins: 9,
+            divWins: 3, 
+        };
+        var team2 = {
+            name: 'Miami Dolphins',
+            overallWins: 6,
+            divWins: 3, 
+        };
+        var team3 = {
+            name: 'Buffalo Bills',
+            overallWins: 4,
+            divWins: 1,
+        };
+        var team4 = {
+            name: 'New York Jets',
+            overallWins: 3,
+            divWins: 0,
+        };
+        var intOrder = [team1, team2, team3, team4];
+        var sortOrder = intOrder.slice(0);
+        sortOrder.sort(function(a,b){
+            return b.overallWins - a.overallWins;
+        })
+        console.log('Standings:');
+        console.log(sortOrder);
+    
     }
-    intOrder.sort(sortTeams);
-    alert(intOrder.join(","));
-
-});
+</script>
 
 
